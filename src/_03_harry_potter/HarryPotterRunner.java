3fae62f507b6dd7e6c5d4f7fc2ba9de125f8f67c
Meme1sh <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-07-18 15:51:03 -0700

    e
    e

---
+++ src/_03_harry_potter/HarryPotterRunner.java
@@ -0,0 +1,11 @@
+package _03_harry_potter;
+
+
+public class HarryPotterRunner {
+	public static void main(String[] args) {
+	new HarryPotter().makeInvisible(true);
+	new HarryPotter().spyOnSnape();
+	new HarryPotter().makeInvisible(false);
+	new HarryPotter().castSpell("Avada Kedavra");
+}
+}

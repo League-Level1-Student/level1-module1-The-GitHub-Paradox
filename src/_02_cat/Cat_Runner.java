0c361518a5f22b1442ab4bb39fa2e567b22ddffa
Meme1sh <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-07-20 10:46:41 -0700

    e
    e

---
+++ src/_02_cat/Cat_Runner.java
@@ -0,0 +1,10 @@
+package _02_cat;
+
+public class Cat_Runner {
+public static void main(String[] args) {
+	new Cat("Chesire").meow();
+	new Cat("Chesire").printName();
+	new Cat("Chesire").kill();	
+}
+
+}
--- src/_04_popcorn/Popcorn_Maker.java
+++ src/_04_popcorn/Popcorn_Maker.java
@@ -8,7 +8,7 @@ public class Popcorn_Maker {
 		String s = JOptionPane.showInputDialog("What flavour do you want your Popcorn to be?");
 		System.out.print("Adding " + s + " flavour to popcorn");
 		String s1 = JOptionPane.showInputDialog("How long do you want your popcorn to be cooked?");
-		System.out.println("Microwave set to " + s1 + "minutes");
+		System.out.println("                           Microwave set to " + s1 + " minutes");
 		System.out.println(s1 + " minutes later");
 		String s2 = JOptionPane.showInputDialog("Eat the " + s + " flavoured popcorn?");
 		if(s2.equals("Yes")) {

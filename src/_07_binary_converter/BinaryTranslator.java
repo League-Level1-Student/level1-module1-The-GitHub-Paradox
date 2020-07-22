aa9c84e9e8cf71d4613eab542d336b1f8b50afee
Meme1sh <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-07-22 10:58:31 -0700

    e
    e

---
+++ src/_07_binary_converter/BinaryTranslator.java
@@ -0,0 +1,48 @@
+package _07_binary_converter;
+
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class BinaryTranslator implements ActionListener {
+public void popup() {
+	JFrame f = new JFrame();
+	JPanel p = new JPanel();
+	JTextField t = new JTextField(20);
+	Button b = new Button();
+	f.add(p);
+	p.add(t);
+	p.add(b);
+	b.addActionListener(this);
+}
+	String convert(String input) {
+	    if(input.length() != 8){
+	        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+	        return "-";
+	    }
+	    String binary = "[0-1]+";//must contain numbers in the given range
+	    if (!input.matches(binary)) {
+	        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+	        return "-";   
+	    }
+	    try {
+	        int asciiValue = Integer.parseInt(input, 2);
+	        char theLetter = (char) asciiValue;
+	        return "" + theLetter;
+	    } catch (Exception e) {
+	        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+	        return "-";
+	    }
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
